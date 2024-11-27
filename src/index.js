50c59760490d3db5149deefc87dbe3f78b4891cb
HusH4All <al394823@uji.es>
2024-11-28 00:15:01 +0100

    Buttons that add roles implemented

--- src/index.js
+++ src/index.js
@@ -14,26 +14,53 @@ client.on("ready", (c) => {
   console.log(`${c.user.tag} is online.`);
 });
 
-client.on("interactionCreate", (interaction) => {
-  if (!interaction.isChatInputCommand()) return;
-
-  if (interaction.commandName === "embed") {
-    const embed = new EmbedBuilder()
-      .setTitle("Embed Title")
-      .setDescription("This is an embed description")
-      .setColor("Random")
-      .addFields({
-        name: "Field title",
-        value: "Some random value",
-        inline: true,
-      },
-      {
-        name: "Field title",
-        value: "Some random value",
-        inline: true,
-      });
-
-    interaction.reply({ embeds: [embed] });
+client.on("interactionCreate", async (interaction) => {
+  try {
+    if (interaction.isButton()) {
+        const role = interaction.guild.roles.cache.get(interaction.customId);
+    
+        await interaction.deferReply({ ephemeral: true });
+        if (!role) {
+            interaction.editReply({
+                content: "I couldn't find that role",
+            })
+            return;
+        }
+    
+        const hasRole = interaction.member.roles.cache.has(role.id);
+    
+        if (hasRole) {
+            await interaction.member.roles.remove(role);
+            await interaction.editReply(`The role ${role} has been removed.`);
+            return;
+        }
+    
+        await interaction.member.roles.add(role);
+        await interaction.editReply(`The role ${role} has been added.`);
+      }
+    
+      else if (!interaction.isChatInputCommand()) return;
+    
+      if (interaction.commandName === "embed") {
+        const embed = new EmbedBuilder()
+          .setTitle("Embed Title")
+          .setDescription("This is an embed description")
+          .setColor("Random")
+          .addFields({
+            name: "Field title",
+            value: "Some random value",
+            inline: true,
+          },
+          {
+            name: "Field title",
+            value: "Some random value",
+            inline: true,
+          });
+    
+        interaction.reply({ embeds: [embed] });
+      }
+  } catch (error) {
+    console.log(error);
   }
 });
 
---
+++ src/send-message.js
@@ -0,0 +1,53 @@
+require("dotenv").config();
+const {
+  Client,
+  IntentsBitField,
+  EmbedBuilder,
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+} = require("discord.js");
+
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
+});
+
+const roles = [
+  {
+    id: "1309231938455994568",
+    label: "Player",
+  },
+];
+
+client.on("ready", async (c) => {
+  try {
+    const channel = await client.channels.cache.get("1311347566843138060");
+    if (!channel) return;
+
+    const row = new ActionRowBuilder();
+    roles.forEach((role) => {
+      row.components.push(
+        new ButtonBuilder()
+          .setCustomId(role.id)
+          .setLabel(role.label)
+          .setStyle(ButtonStyle.Primary)
+      );
+    });
+
+    await channel.send({
+        content: 'Claim or remove a role below.',
+        components: [row],
+    });
+
+    process.exit();
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+client.login(process.env.TOKEN);
