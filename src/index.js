c1c5139a68924c948cb0bcf2b07d35da4d3fdbc0
HusH4All <al394823@uji.es>
2024-11-27 23:41:53 +0100

    First embed approach

--- src/index.js
+++ src/index.js
@@ -1,27 +1,40 @@
-require('dotenv').config();
-const { Client, IntentsBitField } = require('discord.js');
+require("dotenv").config();
+const { Client, IntentsBitField, EmbedBuilder } = require("discord.js");
 
 const client = new Client({
-    intents: [
-        IntentsBitField.Flags.Guilds,
-        IntentsBitField.Flags.GuildMembers,
-        IntentsBitField.Flags.GuildMessages,
-        IntentsBitField.Flags.MessageContent
-    ],
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
 });
 
-client.on('ready', (c) => {
-    console.log(`${c.user.tag} is online.`);
+client.on("ready", (c) => {
+  console.log(`${c.user.tag} is online.`);
 });
 
-client.on('messageCreate', (message) => {
-    if (message.author.bot) {
-        return;
-    }
+client.on("interactionCreate", (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
 
-    if (message.content == 'hello') {
-        message.reply('Hey!');
-    }
+  if (interaction.commandName === "embed") {
+    const embed = new EmbedBuilder()
+      .setTitle("Embed Title")
+      .setDescription("This is an embed description")
+      .setColor("Random")
+      .addFields({
+        name: "Field title",
+        value: "Some random value",
+        inline: true,
+      },
+      {
+        name: "Field title",
+        value: "Some random value",
+        inline: true,
+      });
+
+    interaction.reply({ embeds: [embed] });
+  }
 });
 
 client.login(process.env.TOKEN);
