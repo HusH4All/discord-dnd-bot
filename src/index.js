76bd3d43f0e3f990faf2f4c202302dae16d5655d
HusH4All <al394823@uji.es>
2024-11-28 20:51:23 +0100

    Database + Level System added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const { Client, IntentsBitField, EmbedBuilder, ActivityType } = require("discord.js");
+const mongoose = require('mongoose');
 const eventHandler = require("./handlers/eventHandler");
 
 const client = new Client({
@@ -7,11 +8,21 @@ const client = new Client({
     IntentsBitField.Flags.Guilds,
     IntentsBitField.Flags.GuildMembers,
     IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.GuildPresences,
     IntentsBitField.Flags.MessageContent,
   ],
 });
 
-eventHandler(client);
+(async () => {
+  try {
+    mongoose.set('strictQuery', false);
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log('✅ Connected to Database.');
 
-client.login(process.env.TOKEN);
+    eventHandler(client);
 
+    client.login(process.env.TOKEN);
+  } catch (error) {
+    console.log(`❗Error: ${error}.`)
+  }
+})();
